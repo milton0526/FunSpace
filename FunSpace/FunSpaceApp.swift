86c95ccdc17f6c2c0dad79b3cb561574a76ef6ad
MiltonLiu <90312544+milton0526@users.noreply.github.com>
2022-09-25 16:42:38 +0800

    Initial Commit

---
+++ FunSpace/FunSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunSpaceApp.swift
+//  FunSpace
+//
+//  Created by Milton Liu on 2022/9/25.
+//
+
+import SwiftUI
+
+@main
+struct FunSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
